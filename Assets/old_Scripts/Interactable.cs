bde767302f71812e96b4bffd6a0fbf7b85a385f0
Justin Cain <jwcain@mtu.edu>
2017-10-08 13:42:06 -0400

    Added a folder of scripts from another older project to look at.

---
+++ Assets/old_Scripts/Interactable.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour {
+	public abstract void Interact();
+}
---
+++ Assets/old_Scripts/MovingPlatform.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+	private enum State {
+		A,
+		Wait,
+		B,
+		Wait2
+	}
+	private State state = State.Wait2;
+	private float progress = 0.0f;
+	private Vector3 start;
+	public Vector3 endDisplacemenmt;
+
+	// Use this for initialization
+	void Start () {
+		start = this.transform.position;
+	}
+
+	private void Progress() {
+		progress += Time.fixedDeltaTime * 0.5f;
+		if (progress > 1.0f) {
+			if (state == State.A)
+				state = State.Wait;
+			else if (state == State.B)
+				state = State.Wait2;
+			else if (state == State.Wait)
+				state = State.B;
+			else if (state == State.Wait2)
+				state = State.A;
+			progress = 0.0f;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		if (state == State.A) {
+			this.transform.position = Vector3.Lerp(start, start + endDisplacemenmt, progress);
+		} else if (state == State.B) {
+			this.transform.position = Vector3.Lerp(start + endDisplacemenmt, start, progress);
+		} 
+		Progress();
+	}
+}
