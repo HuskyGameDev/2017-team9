381b6d7b3194dedeb5e99c08d80df5d27a30dfc1
Joe Wright <jvwright@mtu.edu>
2017-12-07 13:16:23 -0500

    Opening door animations. Slightly bugging but working for the most part.

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	Animator anim;
+	private bool door_open;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		door_open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int x = Screen.width / 2;
+		int y = Screen.height / 2;
+
+		if(Input.GetKeyDown(KeyCode.E)){
+			Ray ray = cam.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				Pickup p = hit.collider.GetComponent<Pickup>();
+				if(p != null){
+					if(door_open){
+						anim.Play("DoorClose");
+						door_open = false;
+					} else {
+						anim.Play("DoorOpen");
+						door_open = true;
+					}
+				}
+			}
+		}
+	}
+}
