7df9ffc5c5b98761cb53603e6e683cc5eda2f5e6
Justin Cain <jwcain@mtu.edu>
2018-03-03 16:42:07 -0500

    Deleted some unnecessary files

--- Assets/Scripts/Unused/old_Scripts/Interactable.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Interactable : MonoBehaviour {
-	public abstract void Interact();
-}
--- Assets/Scripts/Unused/old_Scripts/Interactables/Interactable_RandomColorChange.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Interactable_RandomColorChange : Interactable {
-	public override void Interact() {
-		this.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
-	}
-}
