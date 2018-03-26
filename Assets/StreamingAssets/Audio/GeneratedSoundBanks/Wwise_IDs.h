b09643dd47f8a35dfb1c695dda32032adac68c4d
Andrew Stockero <atstocke@mtu.edu>
2018-03-26 16:52:55 -0400

    Removed wwise and then reintegrated.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,31 +13,26 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BUTTON = 977454165U;
-        static const AkUniqueID DOOR_CLOSE = 128625444U;
-        static const AkUniqueID DOOR_LOCK = 3029008661U;
-        static const AkUniqueID DOOR_OPEN = 535830432U;
-        static const AkUniqueID KEYBOARD_CLICKS = 1081112822U;
-        static const AkUniqueID LIGHT_SWITCH = 790135102U;
-        static const AkUniqueID LINE_END = 392429297U;
-        static const AkUniqueID LINE_START = 4086643362U;
-        static const AkUniqueID PHONE = 3104856687U;
-        static const AkUniqueID PUZZLE_CORRECT = 716673090U;
-        static const AkUniqueID SLIDING_DOOR_CLOSE = 1790472453U;
-        static const AkUniqueID SLIDING_DOOR_OPEN = 3615965583U;
+        static const AkUniqueID DOORCLOSE = 3638457343U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID NEW_SOUNDBANK = 4072029455U;
     } // namespace BANKS
 
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
     } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
 
 }// namespace AK
 
