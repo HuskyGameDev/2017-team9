139d52e3d6ac2e083f4c7aabba62acf83553694f
Justin Cain <jwcain@mtu.edu>
2018-04-17 23:03:47 -0400

    Shuffled around how we start new lines

--- Assets/Scripts/Player/GridMovementController.cs
+++ Assets/Scripts/Player/GridMovementController.cs
@@ -81,8 +81,11 @@ public class GridMovementController : MonoBehaviour {
 			if (GetMovementDirection(out movementDirection)) {
 				GridSquare neighbor;
 				if (CheckLegalRegularMove(movementDirection, out neighbor)) {
-					//Now that we have the movement direction, we need to see if another line is on this connection between nodes.
+					//If we are leaving a component we need to start a new line
+					if (currentSquare.type != GridSquare.GridType.Empty)
+						currentLine = new GridSquare.LineHint();
 
+					//Now that we have the movement direction, we need to see if another line is on this connection between nodes.
 					//If the node we are connecting to is type Empty, then we need to break every other line on it
 					if (neighbor.type == GridSquare.GridType.Empty) {
 						//We use a while loop here to avoid desync on the lines data structure
@@ -95,8 +98,6 @@ public class GridMovementController : MonoBehaviour {
 
 					currentSquare.AddLine(movementDirection, currentLine);
 					StartCoroutine(TransitionToNewSquare(movementDirection));
-					if (neighbor.type != GridSquare.GridType.Empty)
-						currentLine = new GridSquare.LineHint();
 				}
 				else if (CheckLegalBackDownLineMove(movementDirection, out neighbor)) {
 					currentSquare.ClearSingleLine(movementDirection);
