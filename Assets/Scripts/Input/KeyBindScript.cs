825c5000328db179ee162ec4be2d1d2149dd8bc3
Richard Carpenter <rlcarpen@mtu.edu>
2018-04-18 02:12:58 -0400

    Started working on Pause Menu
    Added a half functioning pause menu to Main_Jacob. When you click the pause button, it brings up a menu. The menu isn't interactable atm which is the problem, but it saves the state the player was in, so when you hit the pause button again it resumes. The resume button will use the same code as pause.

--- Assets/Scripts/Input/KeyBindScript.cs
+++ Assets/Scripts/Input/KeyBindScript.cs
@@ -10,27 +10,40 @@ public class KeyBindScript : MonoBehaviour {
     private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
     public Text up, left, down, right, pause;
     private GameObject currentKey;
+    private int Pvalue = 0;
     public Color32 normal = new Color(5, 5, 5, 5);
     public Color32 selected = new Color(255,255,255,255);
     // Use this for initialization
     public Button StartButton;
     public Button Options;
     public Button Quit;
+    public Button Resume; 
     public GameObject Main_Menu;
-    public GameObject Options_Menu; 
+    public GameObject Options_Menu;
+    public GameObject Player;
+    private PlayerControls.PlayerState PreviousState;
 
     void Start () {
 
-        Main_Menu.SetActive(true);
-        Options_Menu.SetActive(false);
-
-        Button start = StartButton.GetComponent<Button>();
-        start.onClick.AddListener(StartGame);
+        if (SceneManager.ReferenceEquals(SceneManager.GetActiveScene(), SceneManager.GetSceneByName("dev_Richy")))
+        {
+            Main_Menu.SetActive(true);
+            Options_Menu.SetActive(false);
+            Button start = StartButton.GetComponent<Button>();
+            start.onClick.AddListener(StartGame);
+        }
+        else
+        {
+            Main_Menu.SetActive(false);
+            Options_Menu.SetActive(false);
+        }
+        
         Button options = Options.GetComponent<Button>();
         options.onClick.AddListener(OptionsMenu);
         Button exit = Quit.GetComponent<Button>();
         exit.onClick.AddListener(GetOut);
-        
+        Button resume = Resume.GetComponent<Button>();
+        resume.onClick.AddListener(PausePlease);
 
         keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up","W")));
         keys.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down", "S")));
@@ -48,7 +61,10 @@ public class KeyBindScript : MonoBehaviour {
    
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown((KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Pause"))))
+        {
+            PausePlease();
+        }
        
     }
 
@@ -96,6 +112,26 @@ public class KeyBindScript : MonoBehaviour {
         Debug.Log("GET THE FUCK OUT OF OUR GAME");
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    void PausePlease()
+    {
+        Debug.Log("I'm trying to Pause");
+        if (Pvalue == 0)
+        {
+            PreviousState = Player.GetComponent<PlayerControls>().state;
+            Player.GetComponent<PlayerControls>().state = PlayerControls.PlayerState.Locked;
+            Main_Menu.SetActive(true);
+            Pvalue = 1;
+        }
+        else
+        {
+            Pvalue = 0;
+            Main_Menu.SetActive(false);
+            Options_Menu.SetActive(false);
+            //Player.GetComponent<GridMovementController>().StartCoroutine("TransitionToPlayer");
+            Player.GetComponent<PlayerControls>().state = PreviousState;
+        }
     }
 
     public void SaveKeys()
